003e4206864716d68a3190cebc4cd48633025f9d
Zidna <yuratamma@gmail.com>
2022-08-20 22:16:04 +0700

    feat(flamethrower): delete

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // 'src/*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  'src/*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
   // '**/*.ts?(x)': () => 'npm run build-types',
-  // '*.json': ['prettier --write'],
+  '*.json': ['prettier --write'],
 };
